8de82b52d28d95e14e6cf74fa1bdf88efae7e652
DancingQuanta <andytheseeker@gmail.com>
2018-02-18 20:36:40 +0000

    First crack at developing class-based code structure. This introduces the base class.

--- usbiss/__init__.py
+++ usbiss/__init__.py
@@ -7,26 +7,36 @@ The technical specification can be found here:
 Some of the code is derived from: https://github.com/waggle-sensor/waggle/
 """
 
-import serial
 import struct
+import serial
 
 __author__ = 'Andrew Tolmie'
 __email__ = 'andytheseeker@gmail.com'
 __version__ = '0.1.0'
 
-def iss_spi_divisor(sck):
-    divisor = (6000000 / sck) - 1
-
-    if int(divisor) != divisor:
-        raise ValueError('Nonintegral SCK divisor.')
-
-    return int(divisor)
-
 
 class USBISS(object):
+    """Base class for USB-ISS.
+    The main purpose of the base class is to manage serial connection between
+    the host and USB-ISS.
 
-    def __init__(self, port, iss_mode, **kwargs):
-        self.iss_mode = iss_mode
+    The base class will be the parent class of child classes that manages a
+    connection between USB-ISS and a device. A child class will set the
+    connection protocol and mimic the methods and properties of usual
+    interface libraries. The mimicry will allows an instance of a child class
+    to be used without any modification of the application using the instance.
+    This is because the application expect the methods or properties of an
+    library used to facilitate an connection.
+    """
+
+    self.module = None
+    self.firmware = None
+    self.iss_mode = None
+    self.cur_iss_mode = None
+    self.serial = None
+
+
+    def __init__(self, port):
 
         # Open serial port
         serial_opts = {"port": port,
@@ -41,57 +51,18 @@ class USBISS(object):
         self.get_iss_info()
         self.get_iss_serial_no()
 
-        if self.iss_mode == 'spi':
-            # Select the SPI mode of USB-ISS's SPI operating mode
-            if 'spi_mode' in kwargs:
-                spi_mode = kwargs.get('spi_mode', 0)
-                if 0 <= spi_mode < 4:
-                    # Expose the the SPI mode to external applications
-                    # where self.mode is same as spidev.SpiDev.mode
-                    self.mode = spi_mode
-
-                    # The USB-ISS SPI modes don't match up with official SPI
-                    # mode
-                    elif self.mode == 1:
-                        spi_mode = 2
-                    elif self.mode == 2:
-                        spi_mode = 1
-                    # Add signal for SPI switch
-                    spi_mode = 0x90 + spi_mode
-                else:
-                    error = ("The value of spi_mode, %s, is not "
-                             "between 0 and 3" % (spi_mode))
-                    raise ValueError(error)
-            else:
-                raise TypeError("Missing argument for spi_mode for SPI"
-                                "operating mode")
-            # Select frequency of USB-ISS's SPI operating mode
-            if 'freq' in kwargs:
-                freq = kwargs.get('freq')
-                sck_divisor = iss_spi_divisor(freq)
-                if not 1 <= sck_divisor < 256:
-                    error = "The value of sck_divisor, %s, is not between 0 and 255" % (sck_divisor)
-                    raise ValueError(error)
-            else:
-                raise TypeError("Missing argument for frequency for SPI"
-                                "operating mode")
-            set_bytes = [spi_mode, sck_divisor]
-            msg = ("Initializing USB-ISS in SPI mode with %s spi_mode and %s "
-                   "sck_divisor" % (spi_mode, sck_divisor))
-            print(msg)
-
-        # Configure USB-ISS
-        self.set_iss_mode(set_bytes)
 
     def open(self):
         """Open Serial port to USB-ISS
         """
         self.serial.open()
 
+
     def close(self):
         """Close Serial port to USB-ISS
         """
         self.serial.close()
+
 
     def get_iss_info(self):
         """ Get information about the USB-ISS
@@ -103,11 +74,9 @@ class USBISS(object):
         self.serial.write(bytearray([0x5A, 0x01]))
         response = struct.unpack('BBB', self.serial.read(3))
         if len(response) == 3:
-            mod_id = "The module ID is %s" % (response[0])
-            firmware = "The firmware version is %s" % (hex(response[1]))
-            mode = "The current operating mode is %s" % (hex(response[2]))
-            msg = mod_id + "\n" + firmware + "\n" + mode
-            print(msg)
+            self.module = response[0]
+            self.firmware = hex(response[1])
+            self.cur_iss_mode = hex(response[2])
         else:
             raise RuntimeError("Could not get version details")
 
@@ -117,12 +86,12 @@ class USBISS(object):
         """
         self.serial.write(bytearray([0x5A, 0x03]))
         # Return 8 bytes serial number
-        response = self.serial.read(8)
-        msg = "The serial number is %s" % (response)
-        print(msg)
+        self.iss_sn = self.serial.read(8)
 
 
     def set_iss_mode(self, set_bytes):
+        """Set the operating protocol of the USB-ISS
+        """
         self.serial.write(bytearray([0x5A, 0x02] + set_bytes))
         response = self.serial.read(2)
         if response[0] == 0:
@@ -136,17 +105,10 @@ class USBISS(object):
                 raise RuntimeError('USB-ISS: Undocumented Error')
 
 
-    def xfer(self, data):
-        # spidev function for transferring bytes to port
-        self.serial.write(bytearray([0x61] + data))
-        response = self.serial.read(1 + len(data))
-        if len(response) != 0:
-            status = response[0]
-            if status == 0:
-                raise RuntimeError('USB-ISS: Transmission Error')
-
-            decoded = [struct.unpack('B', response[i + 1: i + 2])[0] for i in range(0, len(data))]
-            
-            return decoded
-        else:
-            raise RuntimeError('USB-ISS: Transmission Error: No bytes received!')
+    def __repr__(self):
+        return ("The module ID is {}\n"
+                "The firmware version is {}\n"
+                "The current operating mode is {}\n"
+                "The serial number is {}").format(
+                    self.module, self.firmware, self.cur_iss_mode, self.iss_sn
+                )
