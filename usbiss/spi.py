4046639ca6b6dee5abd8d6108bb4a343acceb836
DancingQuanta <andytheseeker@gmail.com>
2018-02-26 13:00:43 +0000

    SPI: Use iss_write/iss_read and decode from baseclass as well as storing command bytes and default SPI configuration in variables.

--- usbiss/spi.py
+++ usbiss/spi.py
@@ -5,10 +5,16 @@ class SPI(USBISS):
     """SPI operating mode of USBISS
     """
 
-    self.mode = None
- 
 
-    def __init__(self, port, spi_mode=None, freq=None):
+    SPI_MODE = 0x90
+    SPI_CMD = 0x61
+
+
+    def __init__(self, port, mode=0, max_speed_hz=3000000):
+
+        self._mode = mode
+        self._max_speed_hz = max_speed_hz
+        self.sck_divisor = 1
 
         # Execute baseclass __init__
         super(SPI, self).__init__(port)
@@ -45,15 +51,14 @@ class SPI(USBISS):
         TODO: enforce rule that up to 63 bytes of data can be sent.
         TODO: enforce rule that there is no gaps in data bytes (what define a gap?)
         """
-        self.serial.write(bytearray([0x61] + data))
-        response = self.serial.read(1 + len(data))
+        self.iss_write([self.SPI_CMD] + data)
+        response = self.iss_read(1 + len(data))
         if len(response) != 0:
             status = response[0]
             if status == 0:
                 raise RuntimeError('USB-ISS: Transmission Error')
 
-            decoded = [struct.unpack('B', response[i + 1: i + 2])[0] for i in range(0, len(data))]
-            return decoded
+            return self.decode(response)
         else:
             raise RuntimeError('USB-ISS: Transmission Error: No bytes received!')
 
