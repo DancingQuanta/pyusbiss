1134a4d470f13d5848b01aee533e8952753062a3
gwdehaan <geertwdehaan@hotmail.nl>
2019-03-09 15:44:08 +0100

    test_i2c_registers(A1) added

--- tests/test_i2c_registers(A1)_mcp23008.py
+++ tests/test_i2c_registers(A1)_mcp23008.py
@@ -11,9 +11,11 @@ Testing the I2C module functions for devices with registers. USBISS (I2C_AD1)
 Hardware :
 MCP23008 IO Expander connected to the USBISS () (no pullup R's)
 Address : 64 ((A0..A2) grounded)
+Test 6 - Connect pin 7 (output) with pin 6 (input) for the loopbacktest
 """
 
 import sys
+import time
 import unittest
 from usbiss import i2c
 
@@ -84,6 +86,22 @@ class I2ctestCase(unittest.TestCase):
         else:
             return False
 
+    def _SetPinOn(self, pin):
+        data = self.mcp23008.readU8(GPIO) 
+        data |= 1 << (pin)
+        self.mcp23008.write8(GPIO,data)
+
+    def _SetPinOff(self, pin):
+        data = self.mcp23008.readU8(GPIO) 
+        data &= ~(1 << (pin))
+        self.mcp23008.write8(GPIO, data)
+
+    def _GetPin(self, pin):
+        data = data = self.mcp23008.readU8(GPIO) 
+        data &= (1 << (pin))
+        data  = data >> (pin)
+        return data
+
     def setUp(self):
         self.i2cchannel = i2c.I2C(Port, Handshaking, Speed)
         print(self.i2cchannel._usbiss.__repr__)
@@ -111,6 +129,28 @@ class I2ctestCase(unittest.TestCase):
     def test5_i2cdevice_writelist(self):
         self.assertTrue(self._writeListreadList([0xFF, 0x00, 0xFF]))
  
+    # test I2C module by writing to MCP23008 pin and reading back the result
+    # Connect pin 7 (output) with pin 6 (input)
+    def test6_i2cdevice_loopback(self):
+        OUTPUTPIN = 7
+        INPUTPIN  = 6
+        ON = 1
+        OFF = 0
+        self.mcp23008.write8(IODIR, 0b01111111)
+        # Set OUTPUTPIN off and check te result
+        self._SetPinOff(OUTPUTPIN)
+        pinstat = self._GetPin(INPUTPIN)
+        self.assertEqual(pinstat, OFF)
+        # Set OUTPUTPIN off and check te result
+        self._SetPinOn(OUTPUTPIN)
+        pinstat = self._GetPin(INPUTPIN)
+        self.assertEqual(pinstat, ON)
+        # if a LED is connected to the OUTPUTPIN.
+        time.sleep(2)
+        # Set OUTPUTPIN off and check te result
+        self._SetPinOff(OUTPUTPIN)
+        pinstat = self._GetPin(INPUTPIN)
+        self.assertEqual(pinstat, OFF)
 
 
 
