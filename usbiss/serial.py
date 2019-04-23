8ca7036b7c41673c3676f2e5994745d4a3b680ec
gwdehaan <geertwdehaan@hotmail.nl>
2019-04-23 19:45:14 +0200

    serial - Documentation added

--- usbiss/serial.py
+++ usbiss/serial.py
@@ -6,13 +6,13 @@
 # Process : 1 - send Transmit command ( optional with bytes to send)
 #           2 - response : ACK or NACK - TxCount - RxCount if RxCount > 0 [RxData 1 .. RxData n ]
 # loosely based on pyserial
-# open() – This will open the serial port
 # close() – This will close the serial port
 # readline() – This will read a string from the serial port
-# read(size) – This will read n number of bytes from the serial port
-# write(data) – This will write the data passed to the function to the serial port
+# serial_read(size) – This will read n number of bytes from the serial port
+# serial_write(data) – This will write the data passed to the function to the serial port
 # in_waiting – This variable holds the number of bytes in the buffer
 # out waiting - Number of bytes in the out buffer
+# USBISS - FIFO
 
 
 """Serial support for USB-ISS"""
@@ -44,19 +44,19 @@ class SERIAL(object):
             brhb = divisor[baudrate][0]
             brlb = divisor[baudrate][1]
         except KeyError:
-            raise ValueError('Serial - unknown baudrate possible : (300, 1200, 2400 .. 1000000')
+            raise ValueError('Serial - unknown baudrate;  possible values : (300, 1200, 2400 .. 1000000')
 
         self._usbiss = usbiss.USBISS(port)
         self._usbiss.mode = [ self.SERIAL, brhb, brlb, 0b10101010] #Configure I/O as input.
         self.buffer=[] # Buffer for readline method
-        print('DEBUG SERIAL, init ready')
-
         time.sleep(1)
 
     def write(self, data):
+        # usbiss write function
         self._usbiss.write_data(data)
         
     def read(self, size):
+        # usbiss read function
         ret  = self._usbiss.read_data(size)
         return ret
 
@@ -68,6 +68,11 @@ class SERIAL(object):
         return dec
 
     def _GetResponseFrame(self):
+        """
+        # Read the first three status bytes
+        # from the buffer
+        # Ack / Nack - rxcount - txcount
+        """
         self.write([self.SERIAL_IO])
         time.sleep(.1)
         resp  = self.read(3)
@@ -78,6 +83,9 @@ class SERIAL(object):
         """
         serial_write
         parameter : string of data to be send over 
+        The transmitbuffer is 30 bytes long. if the data
+        is longer than 30 the string is send in multple
+        blocks.
         """
         buffer = list(map(ord, data))
         n = 0
@@ -88,7 +96,6 @@ class SERIAL(object):
                 n+=1
                 if outw > 0:
                     time.sleep(0.5)
-                    print(str(n), str(outw))
                 else:
                     break
             if len(buffer) == 0:
@@ -96,25 +103,26 @@ class SERIAL(object):
             transmitbuffer = buffer[:30]
             buffer= buffer[30:]
             self._usbiss.write_data([self.SERIAL_IO]+transmitbuffer)
-            time.sleep(2) #9600 baud
-        #    if len(buffer) == 0:
-        #        if n == 1:
-        #            outw = self.out_waiting # Necessary to "clean" the controlblock. otherwise in_waiting
-        #            print('serial_write - out_waiting fired off', str(outw))
-        #        return
+            time.sleep(1)
+
 
     def serial_read(self, size):
         """
-        Read the buffer, always read the full buffer
+        serial_read - bytes read are no longer available for consequtive
+        reads - (FIFO)
         """
         self.write([self.SERIAL_IO])
-        #time.sleep(1)
         resp  = self.read(size)
         data = self.decode(resp)
         return data
     
     def readline(self):
-        # buffer =[]
+        """
+        readline - all data that is available is added to self.buffer. after each read from the
+        serial port self.buffer is checked for newlines. If found the function returns the string 
+        up until the first newline.
+        Subsequently calling readline() will produce the next bufferd line. 
+        """
         n = 0 # No of reads
 
         while(True):
@@ -126,26 +134,24 @@ class SERIAL(object):
                 #Add to buffer
                 self.buffer += com
             for pos, i in enumerate(self.buffer):
-                    #print(chr(i), end='')
+     
                 if i == 10: 
                     line=''
                     linebuf = self.buffer[:pos]
                     self.buffer = self.buffer[pos+1:]
                     for c in linebuf:
                         line += chr(c)
-                    #return line + ' ' +str(n) + '- ' + str(len(self.buffer))          
                     return line
-                    # return line
-
 
 
     @property
     def in_waiting(self):
         """
         Get the number of bytes in the input buffer
+        As the controlframe is deleted (FIFO principle) it is very important 
+        that imediately after in_waiting a serial_read is performed.
         """
         [ack, txcount, rxcount] = self._GetResponseFrame()
-        print(rxcount, txcount)
         if ack != 0xFF:
             raise ValueError('Serial - Read - NACK - Transmissionerror')
         return rxcount
@@ -154,6 +160,8 @@ class SERIAL(object):
     def out_waiting(self):
         """
         Get the number of bytes in the output buffer
+        As the controlframe is deleted (FIFO principle) it is very important 
+        that imediately after in_waiting a serial_read is performed.
         """
         [ack, txcount, rxcount] = self._GetResponseFrame()
         return txcount
