04c5204884b442574dc30dad8e39888b0a34b688
gwdehaan <geertwdehaan@hotmail.nl>
2019-05-13 19:13:33 +0200

    Cleanup and rename

--- usbiss/constants.py
+++
@@ -1,4 +0,0 @@
-# GdH 26-3-2019   
-# USBISS - Constants need to be used by users accros different modules
-# because in contrary to SPI the other modules can ben used in combination
-# with each other.
